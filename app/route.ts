90a2d2ed9b4fc83a6c0bc4ae38abbc7eb8ae8d23
Ambushfall <lukathetormentor@gmail.com>
2024-03-23 23:37:16 +0100

    nodejs runtime

--- app/route.ts
+++ app/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 import puppeteer from 'puppeteer'
 
 export const dynamic = 'force-dynamic'
+export const runtime = 'nodejs';
 
 export async function GET () {
   const res = {
