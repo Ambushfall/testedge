d6ef3f0ced47e4d747052db917f1965df8ac94d8
Ambushfall <lukathetormentor@gmail.com>
2024-03-23 23:29:45 +0100

    force dynamic

--- app/route.ts
+++ app/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'
 import puppeteer from 'puppeteer'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET () {
   const res = {
     _response: new NextResponse(),
